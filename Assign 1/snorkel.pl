0c1065a621f43b60f644647a4bd5587de6acd439
guray27 <gurayraphael27@gmail.com>
2019-08-01 16:46:45 +0800

    added assign 1

---
+++ Assign 1/snorkel.pl
@@ -0,0 +1,64 @@
+person(
+		_,
+		name(garry),
+		_,
+		color(not green),
+		beat(barry)
+	).
+
+person(
+		_,
+		name(larry),
+		_,
+		_,
+		beat(person(_,_,color(yellow),_))
+	).
+
+person(
+		_,
+		_,
+		number(3),
+		color(green),
+		_
+	).
+
+person(
+		position(1),
+		_,
+		number(2),
+		_,
+		_
+	).
+
+person(
+		position(4),
+		name(harry),
+		_,
+		_,
+		_
+	).
+
+person(
+		position(2),
+		_,
+		_,
+		color(green),
+		_
+	).
+
+person(
+		_,
+		name(barry),
+		_,
+		color(yellow),
+		_
+	).
+
+person(
+		_,
+		_,
+		_,
+		color(red),
+		beat(person(_,_,_,color(blue),_))
+	).
+
